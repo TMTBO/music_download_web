0d861de2c7855cf5b3fcc8088883986eacc99b58
tenma <thrillerone@hotmail.com>
2025-05-27 14:19:58 +0800

    changed: adapt docker

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const serverIp = import.meta.env.SERVER_IP || "127.0.0.1";
+const serverPort = import.meta.env.SERVER_PORT || "80";
+const baseURL = `http://${serverIp}:${serverPort}`;
+
 const request = axios.create({
-  baseURL: "http://127.0.0.1:80", // 设置基础域名和端口
+  baseURL,
   timeout: 10000,
 });
 
