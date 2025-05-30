ade3490747f6846e91779fca63ef8ce50c66b69f
tenma <thrillerone@hotmail.com>
2025-05-30 23:44:56 +0800

    updated: upate ci

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 SERVER_IP=${SERVER_IP:-127.0.0.1}
-SERVER_PORT=${SERVER_PORT:-5055}
+SERVER_PORT=${SERVER_PORT:-5050}
 echo "========== 服务启动中 =========="
 echo "SERVER_IP: $SERVER_IP"
 echo "SERVER_PORT: $SERVER_PORT"
-sed -i "s/__SERVER_IP__/$SERVER_IP/g" /usr/share/nginx/html/index.html
-sed -i "s/__SERVER_PORT__/$SERVER_PORT/g" /usr/share/nginx/html/index.html
+sed -i "s|__SERVER_IP__|$SERVER_IP|g" /etc/nginx/conf.d/nginx.conf
+sed -i "s|__SERVER_PORT__|$SERVER_PORT|g" /etc/nginx/conf.d/nginx.conf
 echo "========== 服务已启动 =========="
 exec "$@"
--- src/utils/request.js
+++ src/utils/request.js
@@ -5,12 +5,4 @@ const request = axios.create({
   timeout: 10000,
 });
 
-// request.interceptors.request.use((config) => {
-// const protocol = window.location.protocol === "https:" ? "https" : "http";
-// const serverIp = window.RUNTIME_CONFIG?.SERVER_IP || "127.0.0.1";
-// const serverPort = window.RUNTIME_CONFIG?.SERVER_PORT || "5050";
-// config.baseURL = `${protocol}://${serverIp}:${serverPort}`;
-//   return config;
-// });
-
 export default request;
