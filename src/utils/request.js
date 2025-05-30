409202a082a97173f4ce82c6ffc03664f8c6cc72
tenma <thrillerone@hotmail.com>
2025-05-30 17:15:45 +0800

    updated: upate ci

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
-const serverIp = window.RUNTIME_CONFIG?.SERVER_IP || "127.0.0.1";
-const serverPort = window.RUNTIME_CONFIG?.SERVER_PORT || "5050";
-const baseURL = `http://${serverIp}:${serverPort}`;
-console.log("RUNTIME_CONFIG: ", window.RUNTIME_CONFIG);
-console.log("Base URL for API requests:", baseURL);
+function getBaseURL() {
+  const serverIp = window.RUNTIME_CONFIG?.SERVER_IP || "127.0.0.1";
+  const serverPort = window.RUNTIME_CONFIG?.SERVER_PORT || "5050";
+  return `http://${serverIp}:${serverPort}`;
+}
 
-const request = axios.create({
-  baseURL,
-  timeout: 10000,
-});
+function getRequest() {
+  const baseURL = getBaseURL();
+  console.log("RUNTIME_CONFIG: ", window.RUNTIME_CONFIG);
+  console.log("Base URL for API requests:", baseURL);
+  return axios.create({
+    baseURL,
+    timeout: 10000,
+  });
+}
 
-export default request;
+export default getRequest;
