5f550d92f180a3e9d600642f1daa7e06eeb0d6a9
tenma <thrillerone@hotmail.com>
2025-05-30 22:47:05 +0800

    updated: upate ci

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const request = axios.create({
+  baseURL: "/music",
   timeout: 10000,
 });
 
-request.interceptors.request.use((config) => {
-  const protocol = window.location.protocol === "https:" ? "https" : "http";
-  const serverIp = window.RUNTIME_CONFIG?.SERVER_IP || "127.0.0.1";
-  const serverPort = window.RUNTIME_CONFIG?.SERVER_PORT || "5050";
-  config.baseURL = `${protocol}://${serverIp}:${serverPort}`;
-  return config;
-});
+// request.interceptors.request.use((config) => {
+// const protocol = window.location.protocol === "https:" ? "https" : "http";
+// const serverIp = window.RUNTIME_CONFIG?.SERVER_IP || "127.0.0.1";
+// const serverPort = window.RUNTIME_CONFIG?.SERVER_PORT || "5050";
+// config.baseURL = `${protocol}://${serverIp}:${serverPort}`;
+//   return config;
+// });
 
 export default request;
