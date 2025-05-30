8a0b1e6ba2273b7914b9e48f6e3c60c3ca1588eb
tenma <thrillerone@hotmail.com>
2025-05-30 16:54:44 +0800

    updated: upate ci

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const serverIp = window.RUNTIME_CONFIG?.SERVER_IP || "127.0.0.1";
 const serverPort = window.RUNTIME_CONFIG?.SERVER_PORT || "5050";
 const baseURL = `http://${serverIp}:${serverPort}`;
-
+console.log("RUNTIME_CONFIG: ", window.RUNTIME_CONFIG);
 console.log("Base URL for API requests:", baseURL);
 
 const request = axios.create({
