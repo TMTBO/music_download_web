190558d55a9f9d211c3480da9f515b1f9045ec79
tenma <thrillerone@hotmail.com>
2025-05-31 14:58:31 +0800

    updated: update for docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ SERVER_PORT=${SERVER_PORT:-5050}
 echo "========== 服务启动中 =========="
 echo "SERVER_IP: $SERVER_IP"
 echo "SERVER_PORT: $SERVER_PORT"
-sed -i "s|__SERVER_IP__|$SERVER_IP|g" /etc/nginx/conf.d/nginx.conf
-sed -i "s|__SERVER_PORT__|$SERVER_PORT|g" /etc/nginx/conf.d/nginx.conf
+sed -i "s|__SERVER_IP__|$SERVER_IP|g" /etc/nginx/conf.d/default.conf
+sed -i "s|__SERVER_PORT__|$SERVER_PORT|g" /etc/nginx/conf.d/default.conf
 echo "========== 服务已启动 =========="
 exec "$@"
